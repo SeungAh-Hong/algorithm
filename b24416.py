0b63e549ac4ad6088e3f6a8bc53f9136591f1726
SeungAh-Hong <ghdtmddk1516@naver.com>
2022-11-01 12:25:45 +0900

    delete ;

--- b24416.py
+++ b24416.py
@@ -2,7 +2,7 @@ def fib(n):
     if (n==1 or n==2):
         return 1;
     else:
-        return (fib(n-1)+fib(n-2));
+        return (fib(n-1)+fib(n-2))
 
 def fibo(n):
     dp=[0]*(n+1)
@@ -10,7 +10,7 @@ def fibo(n):
     cnt=0
     for i in range(3, n+1):
         cnt+=1
-        dp[i]=dp[i-1]+dp[i-2];
+        dp[i]=dp[i-1]+dp[i-2]
     return cnt
 
 n = int(input())
