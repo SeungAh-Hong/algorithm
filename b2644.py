aab60519774ad14cadb321ee7888640954601613
SeungAh-Hong <ghdtmddk1516@naver.com>
2022-11-22 12:38:34 +0900

    백준 2644. 촌수계산 (BFS)

--- b2644.py
+++ b2644.py
@@ -1,35 +1,29 @@
+import sys
 from collections import deque
-n = int(input())
-a, b = map(int, input().split())
-m = int(input())
+input = sys.stdin.readline
 
+node = int(input())
+start, end = map(int, input().split())
+graph = [[0]*(node+1) for _ in range(node+1)]
 
-## a에서 b까지 line 개수 계산, 없으면 -1
-def BFS(start, end):
-    cnt = 0
+line = int(input())
+for i in range(line):
+    a, b = map(int, input().split())
+    graph[a][b] = graph[b][a] = 1
+
+## start 0촌, 멀어질 수록 +1촌
+chonsu = [0]*(node+1)
+def BFS(idx):
     queue = deque()
-    queue.append(start)
-    depth[start]=0
+    queue.append(idx)
     while queue:
         idx = queue.popleft()
         if idx == end:
-            return depth[end]
-        for i in range(1, n+1):
-            if graph[idx][i]==1 and depth[i]==0:
-                cnt+=1
-                depth[i] = depth[idx]+1
+            return chonsu[end]
+        for i in range(1, node+1):
+            if chonsu[i] == 0 and graph[idx][i] == 1:
+                chonsu[i] = chonsu[idx]+1
                 queue.append(i)
-        
+    return -1
 
-
-graph = [[0]*(n+2) for _ in range(n+2)]
-depth = [0]*(n+2)
-for i in range(1, m+1):
-    x, y = map(int, input().split())
-    graph[x][y] = graph[y][x] = 1
-
-answer = BFS(a, b)
-if answer==None:
-    print(-1)
-else:
-    print(answer)
+print(BFS(start))
