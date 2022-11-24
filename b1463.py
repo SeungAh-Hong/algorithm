5f1f9a3c1ed94e5e754fe55e9a32340b95a246d8
SeungAh-Hong <ghdtmddk1516@naver.com>
2022-11-24 15:21:50 +0900

    백준 1463 1로 만들기

--- b1463.py
+++ b1463.py
@@ -1,16 +1,15 @@
 n = int(input())
-
 dp = [0]*(n+1)
 
-
-# 10 : 10->5->4->2->1 (4)
-# 10 : 10->9->3->1 (3)
-
-for i in range(2, n+1): # Bottom-Up
-    dp[i] = dp[i-1]+1
-    if i%3==0:
-        dp[i] = min(dp[i], dp[i//3]+1) # 비교
-    if i%2==0:
-        dp[i] = min(dp[i], dp[i//2]+1) # 비교
+for i in range(2, n+1):
+    if i%2==0 and i%3==0:
+        dp[i] = min(dp[i-1]+1, dp[i//2]+1, dp[i//3]+1)
+    elif i%2==0:
+        dp[i] = min(dp[i-1]+1, dp[i//2]+1)
+    elif i%3==0:
+        dp[i] = min(dp[i-1]+1, dp[i//3]+1)
+    else:
+        dp[i] = dp[i-1]+1
+    
 
 print(dp[n])
