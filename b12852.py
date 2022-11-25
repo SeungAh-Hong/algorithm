2e901555e6392dbebfd98e8188c0ab9e36d6c78d
SeungAh-Hong <ghdtmddk1516@naver.com>
2022-11-25 12:01:18 +0900

    백준 12852 1로 만들기 2 (DP)

---
+++ b12852.py
@@ -0,0 +1,20 @@
+n = int(input())
+dp = [0]*(n+1)
+history = [0]*(n+1)
+for i in range(2, n+1):
+    dp[i] = dp[i-1]+1
+    history[i] = i-1
+
+    if i%3==0 and dp[i] > dp[i//3]+1:
+        dp[i] = dp[i//3]+1
+        history[i] = i//3
+    if i%2==0 and dp[i] > dp[i//2]+1:
+        dp[i] = dp[i//2]+1
+        history[i] = i//2
+
+print(dp[n])
+i=n
+print(n, end=' ')
+while i!=1:
+    print(history[i], end=' ')
+    i = history[i]
