fd20094c0f0e26e12f9ecb9f355374ec55dec2c3
SeungAh-Hong <ghdtmddk1516@naver.com>
2022-12-01 14:01:16 +0900

    백준 5014 스타트링크 (BFS) next 수정

--- b5014.py
+++ b5014.py
@@ -10,13 +10,7 @@ f, start, end, up, down = map(int, input().split())
 ## start => end 까지의 최소값
 visit = [0]*(f+1)
 queue = deque()
-isUp = 0
-isDown = 0
-
-if up:
-    isUp = 1
-if down:
-    isDown = 1
+ud = [up, (-down)]
 
 def circle(start):
     global visit
@@ -25,21 +19,20 @@ def circle(start):
         idx = queue.popleft()
         if idx == end:
             break
-        nup = idx+up
-        ndown = idx-down
-        if 0< nup <= f and visit[nup]==0 and isUp == 1: ## f층까지밖에 못올라감 !!!!!
-            visit[nup]=visit[idx]+1
-            #print("nup",nup,"visit[nup]",visit[nup])
-            queue.append(nup)
-        if f >= ndown > 0 and visit[ndown]==0 and isDown == 1: ## 0층은 못내려감!!
-            visit[ndown]=visit[idx]+1
-            #print("ndown",ndown,"visit[ndown]",visit[ndown])
-            queue.append(ndown)
+        
+        for i in range(2):
+            next = idx + ud[i]
+            if i==0 and up==0:
+                continue
+            if i==1 and down==0:
+                continue
+            if 0 < next <= f and visit[next] == 0:
+                visit[next] = visit[idx]+1
+                queue.append(next)
 
             
 circle(start)
 
-# print(visit)
 if start == end:
     print(0)
 elif visit[end] == 0:
