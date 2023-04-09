d9bb9e14cc30392f90bace476c1644a0813acbae
SeungAh Hong <110337062+SeungAh-Hong@users.noreply.github.com>
2023-04-09 17:50:35 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 56 ms, Memory: 32820 KB -BaekjoonHub

---
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.py
@@ -0,0 +1,24 @@
+# [14465] 소가 길을 건너간 이유 5 (실버 2)
+import sys
+input = sys.stdin.readline
+# 1이 연속 K개가 되기 위해 고쳐야 하는 최소 신호등의 수
+N, K, B = map(int, input().split())
+lights = [-1]+[1]*(N)
+for b in range(B):
+    err = int(input())
+    lights[err] = 0
+
+tmp=sum(lights[1:K+1])
+ans = K-tmp
+for r in range(K+1, N+1):
+    tmp += lights[r]
+    tmp -= lights[r-K]
+    ans = min(ans, K-tmp)
+    if ans <= 0:
+        break
+
+
+if ans <= 0:
+    print(0)
+else:
+    print(ans)
