e9300748043437babaf34fe378e0317e52e3e803
SeungAh Hong <110337062+SeungAh-Hong@users.noreply.github.com>
2023-04-24 21:16:16 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,27 +1,15 @@
-## 1149 RGB 거리 (DP)
+# [1149] RGB 거리
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-cost = []
-for i in range(n):
-    cost.append(list(map(int, input().split())))
+N = int(input())
+dp = [[0]*3]*N
 
-# dp = [[0]*n for _ in range(n)]
-# ## 모든 경우의 수 구함
-# dp[0][0] = cost[0][0]
-# dp[0][1] = cost[0][1]
-# dp[0][2] = cost[0][2]
+dp=[list(map(int, input().split())) for _ in range(N)]
 
-# for i in range(1, n):
-#     dp[i][0] = min(dp[i-1][1]+cost[i][0], dp[i-1][2]+cost[i][0])
-#     dp[i][1] = min(dp[i-1][0]+cost[i][1], dp[i-1][2]+cost[i][1])
-#     dp[i][2] = min(dp[i-1][0]+cost[i][2], dp[i-1][1]+cost[i][2])
-    
-for i in range(1, n):
-    cost[i][0] += min(cost[i-1][1], cost[i-1][2])
-    cost[i][1] += min(cost[i-1][0], cost[i-1][2])
-    cost[i][2] += min(cost[i-1][0], cost[i-1][1])
+for i in range(1, N):
+    dp[i][0] = min(dp[i-1][1]+dp[i][0], dp[i-1][2]+dp[i][0])
+    dp[i][1] = min(dp[i-1][0]+dp[i][1], dp[i-1][2]+dp[i][1])
+    dp[i][2] = min(dp[i-1][0]+dp[i][2], dp[i-1][1]+dp[i][2])
 
-    
-print(min(cost[n-1]))
+print(min(dp[N-1]))
