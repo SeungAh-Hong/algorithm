443e21814b91090aa7a3a91ef71e170269bf84c5
SeungAh Hong <110337062+SeungAh-Hong@users.noreply.github.com>
2023-10-07 19:30:16 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,19 +1,11 @@
 # [15650] N과M(2) (실버3)
-# import itertools
-# n, m = map(int, input().split())
-# arr = [i for i in range(1,n+1)]
-# arr = map(str, arr)
-# nCr = list(map(' '.join, itertools.combinations(arr, m)))
-
-# nCr = list(nCr)
-
-# for num in nCr:
-#     print(num)
-
-## 다른 풀이
-from itertools import combinations
+import itertools
 n, m = map(int, input().split())
-for i in combinations([i for i in range(1, n+1)], m):
-    print(*i)
+arr = [i for i in range(1,n+1)]
+arr = map(str, arr)
+nCr = list(map(' '.join, itertools.combinations(arr, m)))
 
-# print(*): 리스트 원소나 문자열 각각의 문자를 한 칸 씩 띄운 후(공백) 출력
+nCr = list(nCr)
+
+for num in nCr:
+    print(num)
