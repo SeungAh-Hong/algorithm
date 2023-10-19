243257c686c00055c99dff884572bc13d83e3e18
SeungAh Hong <110337062+SeungAh-Hong@users.noreply.github.com>
2023-10-19 17:47:51 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,16 +1,13 @@
-SELECT a.car_id, a.car_type, (case when c.duration_type NOT IN ('90일 이상') THEN round((a.daily_fee*30)*(1-(c.discount_rate*0.01)),0) ELSE a.daily_fee*30 END) as fee
+SELECT a.car_id, a.car_type, round((a.daily_fee * 30 * (1 - c.discount_rate*0.01)) , 0) as FEE
 FROM car_rental_company_car a
 JOIN car_rental_company_rental_history b ON a.car_id = b.car_id
 JOIN car_rental_company_discount_plan c ON a.car_type = c.car_type
-WHERE a.car_type IN ('SUV', '세단') and
-    a.car_id NOT IN (
-        SELECT car_id
-        FROM car_rental_company_rental_history
-        WHERE start_date BETWEEN '2022-11-01' AND '2022-11-30'
-        OR end_date BETWEEN '2022-11-01' AND '2022-11-30'
-        OR (start_date <= '2022-11-01' AND end_date >= '2022-11-30')
-    )
-    and duration_type like '30일 이상'
-GROUP BY a.car_id
-HAVING fee BETWEEN 500000 AND 2000000
-ORDER BY fee DESC, car_type, car_id DESC
+where a.car_type in ('세단', 'SUV') and c.duration_type = '30일 이상'
+    and a.car_id not in (
+    select car_id
+    from car_rental_company_rental_history
+    where start_date between '2022-11-01' and '2022-11-30'
+    or end_date between '2022-11-01' and '2022-11-30'
+    or (start_date <= '2022-11-01' and end_date >= '2022-11-30'))
+group by a.car_id having fee between 500000 and 2000000
+order by fee desc, car_type, car_id desc
